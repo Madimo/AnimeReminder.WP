f7b458235b4edbd26f9f50ff638a8d32ddc1275e
Madimo <MadimoZhang@gmail.com>
2013-12-14 15:42:10 +0800

    finish ClearCache

--- NewAnimeChecker/GeneralSettingsPage.xaml.cs
+++ NewAnimeChecker/GeneralSettingsPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Phone.Controls;
+using System.Diagnostics;
 using System.IO.IsolatedStorage;
 using System.Windows;
 using System.Windows.Media;
+using Coding4Fun.Toolkit.Controls;
 
 namespace NewAnimeChecker
 {
@@ -40,5 +42,36 @@ namespace NewAnimeChecker
         {
             Pivot.Background = (ImageBrush)App.Current.Resources["BackgroundBrush"];
         }
+
+        #region 清除图片缓存
+        private void ClearCache_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("", "确定清除图片缓存？", MessageBoxButton.OKCancel) == MessageBoxResult.Cancel)
+                return;
+
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                try
+                {
+                    string[] files = isf.GetFileNames("/Cache/*.jpg");
+                    foreach (string file in files)
+                    {
+                        isf.DeleteFile("/Cache/" + file);
+                    }
+                    ToastPrompt toast = new ToastPrompt()
+                    {
+                        Title = "成功清除图片缓存",
+                        FontSize = 20
+                    };
+                    toast.Show();
+                }
+                catch
+                {
+                    MessageBox.Show("部分缓存删除失败");
+                }
+            }
+        }
+        #endregion
+
     }
 }
